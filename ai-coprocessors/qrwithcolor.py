9f5e8fb7f1d5430b8639cbe35d78eb24a899c96d
staberas <staberas@users.noreply.github.com>
2020-10-22 08:06:41 +0300

    Add files via upload

---
+++ ai-coprocessors/qrwithcolor.py
@@ -0,0 +1,33 @@
+import sensor
+import image
+import lcd
+import time
+
+clock = time.clock()
+lcd.init()
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.set_vflip(1)
+sensor.set_hmirror(1) # set hmirror if can not recognize qr code
+sensor.skip_frames(30)
+print('start qr')
+green_threshold   = (0,   80,  -70,   -10,   -0,   30)
+while True:
+    clock.tick()
+    img = sensor.snapshot()
+    res = img.find_qrcodes()
+    fps = clock.fps()
+    if len(res) > 0:
+        img.draw_string(2,2, res[0].payload(), color=(0,128,0), scale=2)
+        print(res[0].payload())
+        img.draw_rectangle(res[0].x(),res[0].y(),res[0].w(),res[0].h(),color=(255,1,1))
+        print(res[0])
+    blobs = img.find_blobs([green_threshold])
+    if blobs:
+        for b in blobs:
+            tmp=img.draw_rectangle(b[0:4])
+            tmp=img.draw_cross(b[5], b[6],color=(255,1,1))
+            c=img.get_pixel(b[5], b[6])
+            #print(b.area())
+    lcd.display(img)
